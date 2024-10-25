e426c265f95f2694016dab42e157538dfd85f761
Thanula Snadaruwan Rajapaksha <113056152+carnage2003@users.noreply.github.com>
2024-10-25 06:28:51 +0530

    just a test

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            Console.WriteLine("FAT ESTIMATION");
+
+            Console.WriteLine("ENTER GENDER"); 
+            String sex= (Console.ReadLine());
+
+            Console.WriteLine("ENTER BMI");
+            float bmi= (Console.ReadLine());
+
+            Console.WriteLine("ENTER AGE");
+            int age= (Console.ReadLine());
+
+
+               
+
+
+
+        }
+    }
+}
